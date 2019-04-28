9c416ee60a36fdbff29918de055b21ddac888d13
LanTastiic <42815592+LanTastiic@users.noreply.github.com>
2019-04-27 19:58:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var list = document.getElementsByClassName("new-item");
+
+function createList() {
+    var li = document.createElement(list);
+    ul.appendChild(li);
+}
+
+function getFirstName() {
+    var firstName = document.getElementById("first-name").value;
+}
+
+
+document.getElementById("list-button").onclick = function () {
+    location.href = "createListing.html";
+};
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("list-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
